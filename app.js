f70e2a242ad8b6c9e68c35ebe9d7bff5afc46491
Viorel <viorel_popa89@yahoo.com>
2021-05-06 15:01:29 +0300

    Fix layout, Fix 404

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/Geolocation');
 var usersRouter = require('./routes/users');
+const { resolve } = require('path');
+const { rejects } = require('assert');
+const { response } = require('express');
 
 var app = express();
 
@@ -24,18 +27,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.status(404).send('Sorry bro, nothing to see here');
+  next(createError(404));
 });
 
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
