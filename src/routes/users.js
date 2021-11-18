4b9b1accc05e20c652b64eb2ed7025f350f8115e
Viorel <viorel_popa89@yahoo.com>
2021-11-18 08:37:06 +0200

    check for credentials in db and send message if they validate

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const user = require("../db/models/user");
 var router = express.Router();
 
 //Define dummy-data users
@@ -19,14 +20,37 @@ router.get("/login", function (req, res) {
 
 /* POST login form data */
 router.post("/login", function (req, res) {
-  if (req.body.name === name && req.body.password === password) {
-    session = req.session;
-    session.userid = req.body.name;
-    return res.redirect("/");
-  } else {
+
+  // handle user login using sequelize
+  const db = req.app.locals.db;
+
+  db.User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+  .then(user => {
+    if (user.password === req.body.password) {
+      res.send("It's alive"); 
+    } else {
+      res.send("Sorry bro...");
+    }
+  })
+
+  .catch((err) => {
+    console.log(err);
     res.render("login");
-  }
-});
+  })
+
+  //initialize session
+//   if (req.body.name === name && req.body.password === password) {
+//     session = req.session;
+//     session.userid = req.body.name;
+//     return res.redirect("/");
+//   } else {
+//     res.render("login");
+//   }
+ });
 
 /* GET registration form. */
 router.get("/register", function (req, res) {
